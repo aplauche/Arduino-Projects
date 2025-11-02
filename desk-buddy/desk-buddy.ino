9d3e197faa2b7641817a0e70d8066bac8788503d
Anton Plauche <antonplauche92@gmail.com>
2025-11-02 15:31:40 -0600

    working white noise

--- desk-buddy/desk-buddy.ino
+++ desk-buddy/desk-buddy.ino
@@ -2,10 +2,18 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
 #include "DFRobot_DF2301Q.h"
+#include "DFRobotDFPlayerMini.h"
 #define NUM_LEDS 48
+#include <SoftwareSerial.h>
+
 CRGB leds[NUM_LEDS];
 
 rgb_lcd lcd;
+
+// Assign MP3 Player Serial
+#define FPSerial Serial1
+//SoftwareSerial FPSerial(8, 9);  // RX=8, TX=9
+DFRobotDFPlayerMini myDFPlayer;
 
 // Color Array
 CHSV colors[] = {
@@ -56,12 +64,31 @@ int currentMenu = 0;  // 0 = main, 1 = Lamp Color, 2 = Breath, 3 = Focus Timer
 int currentItem = 0;
 
 // Menu data
-String mainMenu[] = {"Lamp Color", "Breath & Relax", "Focus Timer"}; // 0
+String mainMenu[] = {"Lamp Color", "Breath & Relax", "Focus Timer", "White Noise"}; // 0
 String colorMenu[] = {"Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Purple", "Pink"}; // 1
 String breathMenu[] = {"5 Breaths", "10 Breaths"}; // 2
 String focusMenu[] = {"10 Minutes", "15 Minutes", "20 Minutes"}; // 3
+String noiseMenu[] = {"On", "Off"}; // 2
 
 void setup() { 
+  Wire.end();
+  FPSerial.begin(9600);
+
+  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
+
+  myDFPlayer.volume(20);  //Set volume value. From 0 to 30
+
+  Wire.begin();
+
+
   FastLED.addLeds<WS2812, 6, GRB>(leds, NUM_LEDS); 
 
   FastLED.setBrightness(LEDBrightness);
@@ -81,6 +108,8 @@ void setup() {
   lcd.print("Hi There :)");
   delay(2000);
   showMenu();
+
+
 
 }
 
@@ -116,8 +145,6 @@ void loop() {
 void reset(){
   currentMenu = 0;
   currentItem = 0;
-  FastLED.setBrightness(LEDBrightness);
-  FastLED.show();
   showMenu();
 }
 
@@ -164,6 +191,8 @@ void boxBreath(){
     breathCount = 0;
     breathTimer = 0;
     breathState = 0;
+    FastLED.setBrightness(LEDBrightness);
+    FastLED.show();
     reset();
     return;
   }
@@ -253,6 +282,25 @@ void handleSelect() {
 
     }
 
+    if(currentMenu == 4){
+      if(currentItem == 0){
+        myDFPlayer.play(1);  //Play the first mp3
+        myDFPlayer.loop(1);  //Loop the first mp3
+        lcd.clear();
+        lcd.print("Noise On!");
+
+      } else {
+        myDFPlayer.pause();
+        lcd.clear();
+        lcd.print("Noise Off!");
+      }
+
+      delay(1000); 
+      reset();
+      return;
+    }
+
+
 
     // Simulate for others...
     lcd.clear();
@@ -265,10 +313,11 @@ void handleSelect() {
 
 
 int getItemCount() {
-  if (currentMenu == 0) return 3;     // main
+  if (currentMenu == 0) return 4;     // main
   if (currentMenu == 1) return 8;     // color
   if (currentMenu == 2) return 2;     // breath
   if (currentMenu == 3) return 3;     // focus timer
+  if (currentMenu == 4) return 2;     // noise
   return 0;
 }
 
@@ -320,5 +369,10 @@ void showMenu() {
     lcd.setCursor(0, 1);
     lcd.print("> ");
     lcd.print(focusMenu[currentItem]);
+  } else if (currentMenu == 4) {
+    lcd.print("White Noise");
+    lcd.setCursor(0, 1);
+    lcd.print("> ");
+    lcd.print(noiseMenu[currentItem]);
   }
 }
